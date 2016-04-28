f8610bb23361d3f8f8a350740479c04269ed2928
an.sokolov <sokolik_85@list.ru>
2016-04-28 18:19:56 +0600

    create a backend side of the chat in server.js

---
+++ server.js
@@ -0,0 +1,44 @@
+/**
+ * Created by an.sokolov on 28.04.2016.
+ */
+
+var http = require('http');
+var fs = require('fs');
+var chat = require('./chat');
+
+http.createServer(function(req,res ) {
+    switch (req.url) {
+        case '/':
+            sendFile('index.html', res);
+            break;
+
+        case '/subscribe':
+            chat.subscribe(req.res);
+
+            //..
+            break;
+
+        case '/publish':
+            chat.publish('...');
+            //...
+            break;
+
+        default:
+            res.statusCode = 404;
+            res.end("Not found");
+    }
+}).listen(3000);
+
+
+function sendFile(fileName,res){
+    var fileStream = fs.createReadStream(fileName);
+    fileStream
+        .on('error', function(){
+            res.statusCode = 500;
+            res.end("Server error");
+        });
+    fileStream.on("end", function() {
+
+        res.end(fileName);
+    })
+}
