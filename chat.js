38a17f82bb07b683428e7b313cde108e6259ef31
an.sokolov <sokolik_85@list.ru>
2016-04-29 18:35:52 +0600

    add connection close handeling (not to keep closed connections in clients arr)

--- chat.js
+++ chat.js
@@ -7,6 +7,10 @@ exports.subscribe = function(req,res){
     console.log('subscribe');
 
     clients.push(res);
+
+    res.on('close', function(){
+        clients.splice(clients.indexOf(res),1)
+    })
 };
 
 exports.publish = function(message){
